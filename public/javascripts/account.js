5a83275c8ea59edbd0fc26b807c4a6d716d3497d
info@uaieee.org <info@uaieee.org>
2022-11-16 03:01:57 +0000

    Loads of changes (front end, mongo)

--- public/javascripts/account.js
+++ public/javascripts/account.js
@@ -1,3 +1,36 @@
 $(function(){
     $("#nav-placeholder").load("navbar.html");
   });
+
+  $("#button").click(addDevice);
+
+  function addDevice(){
+   
+
+    
+    let name = prompt("Enter name of Device: ");
+
+    $.ajax({
+        url: /*FIX Ary we need  the new info*/'device/addDevice',
+        method: 'POST',
+        contentType: 'application/json',
+        data: name,
+        dataType: 'json'
+  
+  
+  
+      })
+      .done(function (data, textStatus, jqXHR) {
+        //Need Mongodb function to retreive updated list of devices for this user. 
+
+        })
+        .fail(function(data, textStatus, errorThrown){
+          alert(JSON.parse(data.responseText).msg);
+          // fixed 
+  
+        })
+
+    
+
+
+  }
